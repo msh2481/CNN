d5d604f0363ac0bc6496b89b9d1ad644e1467b5d
msh2481 <msh2481@ya.ru>
2022-01-27 11:50:53 +0300

    fixed random ranges

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def objective(trial):
         'perspective_distortion': 0.01,
         'cutout_count': 1,
         'cutout_min_size': cutout_min,
-        'cutout_max_size': trial.suggest_int('cutout_max_size', cutout_min, 3 * cutout_min, log=True),
+        'cutout_max_size': trial.suggest_int('cutout_max_size', cutout_min + 1, 3 * cutout_min, log=True),
 
         'model': 'Dummy()',
         'batch_size': bs,
